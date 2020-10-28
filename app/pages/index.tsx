b94046328cfec3074c1f48750c66c95204f581d2
Shaun Church <shaun@shaunchurch.com>
2020-10-28 09:36:29 +0000

    Basic homepage layout

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,15 +1,45 @@
-import { Suspense } from "react"
-import { BlitzPage } from "blitz"
+import { BlitzPage, Link, useQuery } from "blitz"
 import Layout from "app/layouts/Layout"
-import UserInfo from "app/auth/components/UserInfo"
+import getProducts from "app/products/queries/getProducts"
+import { Suspense } from "react"
+
+const HomePage = () => {
+  const [products] = useQuery(getProducts, {})
+  console.log("products", products)
+
+  return (
+    <div className="">
+      <div className="flex flex-col space-y-10 py-20 justify-center text-center">
+        <p className="uppercase tracking-wider text-blue-600">Introducing...</p>
+        <h1 className="text-6xl font-extrabold tracking-tighter">Simple user feedback.</h1>
+        <p className="text-gray-800">Without happy users, you are nothing.</p>
+        <Link href="/signup">
+          <a className="bg-blue-500 hover:bg-blue-700 px-4 py-2 rounded text-white font-bold max-w-sm mx-auto">
+            I want feedback
+          </a>
+        </Link>
+      </div>
+      <ul className="grid grid-cols-12">
+        {products?.products?.map((product) => {
+          return (
+            <li key={product.id} className="border p-4 col-span-4">
+              <h2 className="font-bold text-lg">{product.name}</h2>
+              <Link href={`/products/${product.id}`}>
+                <a className="text-blue-500 underline">View requests</a>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
 
 const Home: BlitzPage = () => {
   return (
-    <div className="container">
-      <Suspense fallback="Loading...">
-        <UserInfo />
-      </Suspense>
-    </div>
+    <Suspense fallback="Loading...">
+      <HomePage />
+    </Suspense>
   )
 }
 
